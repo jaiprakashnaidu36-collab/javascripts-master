aa73245420f622f93ee9df35437fbd7a53c4e8fa
jaiprakashnaidu36-collab <jaiprakashnaidu36@gmail.com>
2026-09-11 21:08:22 +0530

    commit

---
+++ personal/classwork/basics/math.js
@@ -0,0 +1,61 @@
+// The Math.abs() static method returns the absolute value of a number
+
+const vari = -120
+
+console.log(Math.abs(vari));
+
+console.log(Math.abs(-Infinity));
+
+// There are many angle function in JS
+
+/*
+acos()
+acosh()
+asin()          
+asinh()
+atan()
+atan2()
+atanh()
+cos()
+cosh()
+
+syntax for every function is the same
+
+math.<angle>(variable name)
+
+*/
+
+// The Math.ceil() static method always rounds up and returns the smallest integer greater than or equal to a given number.
+
+console.log(Math.ceil(0.95));
+console.log(Math.ceil(7.004));
+
+// The Math.exp() static method returns e raised to the power of a number
+
+console.log(Math.exp(0));     // e^0 = 1
+
+// The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.
+
+console.log(Math.floor(9.9));
+console.log(Math.floor(1.2));
+
+// Math.random() returns a random number inbetween 0 and 1
+
+console.log(Math.random());
+
+
+// The Math.round() static method returns the value of a number rounded to the nearest integer.
+
+console.log(Math.log(0.9));
+console.log(Math.log(1.67));
+
+// The Math.pow() static method returns the value of a base raised to a power
+
+console.log(Math.pow(7, 3));    // 343
+console.log(Math.pow(-7, 0.5))       // NAN because negative numbers don't have a real square root
+
+
+
+
+
+
