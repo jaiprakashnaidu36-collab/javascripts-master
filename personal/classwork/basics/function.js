c58fe1ff48885a77c0ec2f8ef749fbe2f17190e1
jaiprakashnaidu36-collab <jaiprakashnaidu36@gmail.com>
2026-09-16 17:47:25 +0530

    commit

--- personal/classwork/basics/function.js
+++ personal/classwork/basics/function.js
@@ -45,3 +45,49 @@ console.log(fun("jai"));
 
 // If we are passing any data in the parameters and not passing anything in the argument then the defalut value will be printed
 // If both the value is passed in the parameters as well as in the arguments then the value that is passed in the parameters will be overwriten 
+
+// Rest operator -> used when multiple data is being assigned to a single variable in function
+
+function collect(...price){
+    return price;
+}
+
+console.log(collect(200,300,100,400,500));
+
+
+// rest operator will return the value in the form of array
+
+// objects with function
+
+const user = {
+    name : "Jai prakash",
+    age : 19
+}
+
+function UserObj(ObjName){
+    console.log(`User name is ${ObjName.name} and their age is ${ObjName.age}`)
+}
+
+UserObj(user)
+
+
+// we can even pass objects directly into the function call
+
+function defcal(usercall){
+    console.log(`Name = ${usercall.name} , Age = ${usercall.age}`)
+}
+
+defcal({
+    name : "Jai prakash",
+    age : 19
+})
+
+// Arrays passed as function arguments
+
+const arr = [10,20,30,40]
+
+function getarr(arrnm){
+    console.log(`arr = ${arrnm}`)
+}
+
+getarr(arr)
