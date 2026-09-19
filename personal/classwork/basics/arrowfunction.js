80d4f127bef46f9e34b46b3ac68a56dcbdccd67b
jaiprakashnaidu36-collab <jaiprakashnaidu36@gmail.com>
2026-09-19 13:00:48 +0530

    commit

--- personal/classwork/basics/arrowfunction.js
+++ personal/classwork/basics/arrowfunction.js
@@ -2,4 +2,56 @@ const name = () => {
     console.log("hello")
 }
 
-name()
+// name()
+
+// This keyword
+
+/* this refers to the object that is currently executing or calling the function, and its value 
+is typically determined by how a function is called at runtime, not where it was declared
+
+
+*/
+
+
+const obj = {
+    username : "Jai prakash",
+    pass : 98779 , 
+    fun : function(){
+        console.log(`${this.username}`);
+        console.log(this);
+    }
+
+}
+
+// obj.fun()
+
+// console.log(this);            // we get an empty object {}
+
+function nam_(){
+    let userm = "jp"
+    console.log(this.userm)         // we get undefined
+}
+
+// nam_()
+
+const ar = () =>{
+    let co = "Jp"
+    console.log(this.co);
+}
+
+// ar()
+
+
+
+const add = (n1,n2) =>{
+    return n1 + n2
+}
+
+// console.log(add(2,3))
+
+// implicit return in arrow function
+
+const add1 = (n1,n2) => n1 + n2       // implicit return type no need to write return 
+
+console.log(add1(2,3))
+
